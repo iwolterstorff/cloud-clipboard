62468221a9be3c4125231cfbd9e6eaf33d76fd6a
Ian Wolterstorff <wolterstorff.i@husky.neu.edu>
2019-07-29 20:59:15 -0400

    Formatting and remove log

--- static/main.js
+++ static/main.js
@@ -4,9 +4,8 @@ var textArea = document.getElementById("thisiswherethestuffgoes");
 
 textArea.addEventListener("input", function(ev) {
   socket.emit("text", textArea.value);
-  console.log(textArea.value);
 });
 
 socket.on("text", function(data) {
-    textArea.value = data;
+  textArea.value = data;
 });
